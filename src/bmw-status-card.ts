211096108da5068ddc21bf5218a54280a3d332f3
Lukas Weberruß <lukas@klue.de>
2026-02-10 09:30:37 +0100

    Release v0.1.43

--- src/bmw-status-card.ts
+++ src/bmw-status-card.ts
@@ -2,7 +2,7 @@ import { LitElement, css, html } from 'lit';
 
 const CARD_NAME = 'bmw-status-card';
 const VEHICLE_CARD_NAME = 'vehicle-status-card';
-const VERSION = '0.1.41';
+const VERSION = '0.1.43';
 
 type HassState = {
   entity_id: string;
